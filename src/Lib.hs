49760f5d254781f6dde4a4766d22a429ac10cad1
Michal Kawalec <michal@monad.cat>
2018-07-11 18:18:53 +0200

    use a faster rng

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,7 +18,8 @@ import           Data.Tuple.HT (uncurry3)
 import qualified Debug.Trace as DT
 import           GHC.Generics (Generic)
 import           Graphics.ColorSpace
-import           System.Random
+
+import PCG
 
 data Ray = Ray
   { origin    :: !V3
@@ -56,7 +57,7 @@ dot :: V3 -> V3 -> Double
 dot !a !b = let (V3 x y z) = (a * b) in x + y + z
 {-# INLINE dot #-}
 
-color :: Hitable a => Ray -> a -> StdGen -> Int -> V3
+color :: Hitable a => Ray -> a -> PCG32 -> Int -> V3
 color r@(Ray o d) world gen callCount =
   let unitDirection@(V3 x _ _) = unitVector d
       {-# INLINE unitDirection #-}
@@ -64,7 +65,7 @@ color r@(Ray o d) world gen callCount =
       skyColor = asV3 (1.0 - t) + asV3 t * V3 0.5 0.7 1.0
 
       sphereColor :: HitResult -> V3
-      sphereColor hit =
+      sphereColor hit = 
         let (g1, g2) = split gen
         in case scatter g1 r hit of
             Just (attenuation, scattered) ->
@@ -127,7 +128,7 @@ instance Hitable Sphere where
           negativeRoot = (-b - (sqrt $ b*b - a*c)) / a
   {-# INLINE hit #-}
 
-scatter :: StdGen -> Ray -> HitResult -> Maybe (V3, Ray)
+scatter :: PCG32 -> Ray -> HitResult -> Maybe (V3, Ray)
 scatter gen ray@(Ray origin direction) (HitResult _ p normal (Lambertian albedo)) =
   let target = p + normal + randomInUnitSphere gen
       scattered = Ray p (target - p)
@@ -142,12 +143,13 @@ scatter gen ray@(Ray origin direction) (HitResult _ p normal (Metal albedo fuzz)
                                    else Nothing
 {-# INLINE scatter #-}
 
-randomInUnitSphere :: StdGen -> V3
-randomInUnitSphere gen = unitVector p
-  where (v1, g1) = random gen
-        (v2, g2) = random g1
-        (v3, _) = random g2
-        p = asV3 2.0 * (V3 v1 v2 v3) - asV3 1
+randomInUnitSphere :: PCG32 -> V3
+randomInUnitSphere gen = 
+  let (v1, g1) = nextD gen
+      (v2, g2) = nextD g1
+      (v3, _) = nextD g2
+      p = asV3 2 * (V3 v1 v2 v3) - asV3 1
+  in unitVector p
 {-# INLINE randomInUnitSphere #-}
 
 arrLightIx2 :: Hitable a => Ix2 -> World a -> Int -> Image S RGB Double
@@ -161,10 +163,11 @@ arrLightIx2 arrSz@(sizeY :. sizeX) world samples =
 
         castRay :: Int -> Int -> V3 -> Int -> V3
         castRay !i !j !rgb !sample =
-          let idx = 2 * (i + 1) * (j + 1) * (sample + 1)
-              generator = mkStdGen idx
-              (r1, g1) = random generator
-              (r2, g2) = random g1
+          let 
+              idx = 2 * (i + 1) * (j + 1) * (sample + 1)
+              stdGen = newPCG32 (fromIntegral idx) (fromIntegral idx)
+              (r1, g1) = nextD stdGen
+              (r2, g2) = nextD g1
               u = (r1 + fromIntegral i) / fromIntegral sizeX
               v = (r2 + fromIntegral (sizeY - 1 - j)) / fromIntegral sizeY
 
@@ -180,7 +183,6 @@ arrLightIx2 arrSz@(sizeY :. sizeX) world samples =
 
 someFunc :: IO ()
 someFunc = do
-  stdGen <- getStdGen
   let world =
         World $
         fromList Seq
@@ -189,5 +191,5 @@ someFunc = do
           , (Sphere (V3 1        0 (-1)) 0.5 (Metal (V3 0.8 0.6 0.2) 0.3))
           , (Sphere (V3 (-1)     0 (-1)) 0.5 (Metal (V3 0.8 0.6 0.8) 1.0))
           ]
-      img = arrLightIx2 (800 :. 1600) world 100
+      img = arrLightIx2 (300 :. 600) world 100
   writeImage "light.png" img
---
+++ src/PCG.hs
@@ -0,0 +1,179 @@
+{-# LANGUAGE BangPatterns       #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+-- |
+--
+-- Standard PCG32 Random Number Generator with chosen streams, written in
+-- pure haskell. See <http://www.pcg-random.org> for details.
+--
+module PCG 
+  ( -- * PCG 32
+    PCG32 (..)
+  , seed
+  , newPCG32
+
+    -- * Generating random numbers
+  , next32
+  , next64
+  , nextD
+  , bounded
+
+    -- * Generator utilities
+  , advancePCG32
+  , split
+  ) where
+
+import Data.Bits
+import Data.Data
+import Foreign
+import GHC.Generics
+import Data.Binary.IEEE754
+
+-- | The multiple sequence varient of the pcg random number generator.
+data PCG32 = PCG32
+  {-# UNPACK #-} !Word64 -- state
+  {-# UNPACK #-} !Word64 -- inc
+  deriving (Show, Ord, Eq, Data, Typeable, Generic)
+
+-- | Create a new generator from two words.
+newPCG32 :: Word64 -> Word64 -> PCG32
+newPCG32 = \a b ->
+  let s = state (PCG32 (a + i) i)
+      i = (b `shiftL` 1) .|. 1
+  in  PCG32 s i
+{-# INLINE newPCG32 #-}
+
+-- | Fixed seed.
+seed :: PCG32
+seed = PCG32 9600629759793949339 15726070495360670683
+{-# INLINE seed #-}
+
+-- Generating random numbers -------------------------------------------
+
+-- All operations are done via Pair to ensure everything's strict. GHC
+-- is good at inlining this so Pair doesn't exist in the generated core.
+data Pair = Pair
+  {-# UNPACK #-} !Word64 -- new state
+  {-# UNPACK #-} !Word32 -- output
+
+multiplier :: Word64
+multiplier = 6364136223846793005
+{-# INLINE multiplier #-}
+
+-- A single step in the generator state
+state :: PCG32 -> Word64
+state (PCG32 s inc) = s * multiplier + inc
+{-# INLINE state #-}
+
+-- The random number output
+output :: Word64 -> Word32
+output s =
+  (shifted `unsafeShiftR` rot) .|. (shifted `unsafeShiftL` (negate rot .&. 31))
+  where
+    rot     = fromIntegral $ s `shiftR` 59 :: Int
+    shifted = fromIntegral $ ((s `shiftR` 18) `xor` s) `shiftR` 27 :: Word32
+{-# INLINE output #-}
+
+-- a new generator state and random number
+pair :: PCG32 -> Pair
+pair g@(PCG32 s _) = Pair (state g) (output s)
+{-# INLINE pair #-}
+
+-- | Return a random 'Word32' with a generator incremented by one step.
+next32 :: PCG32 -> (Word32, PCG32)
+next32 = \g@(PCG32 _ inc) ->
+  let Pair s' r = pair g
+  in  (r, PCG32 s' inc)
+{-# INLINE next32 #-}
+
+-- | Return a random 'Word64' with the generator incremented by two steps.
+next64 :: PCG32 -> (Word64, PCG32)
+next64 = \(PCG32 s inc) ->
+  let Pair s'  w1 = pair (PCG32 s inc)
+      Pair s'' w2 = pair (PCG32 s' inc)
+  in  (wordsTo64Bit w1 w2, PCG32 s'' inc)
+{-# INLINE next64 #-}
+
+wordsToDouble :: Word32 -> Word32 -> Double
+wordsToDouble x y  = (fromIntegral u * m_inv_32 + (0.5 + m_inv_53) +
+                     fromIntegral (v .&. 0xFFFFF) * m_inv_52)
+    where m_inv_52 = 2.220446049250313080847263336181640625e-16
+          m_inv_53 = 1.1102230246251565404236316680908203125e-16
+          m_inv_32 = 2.3283064365386962890625e-10
+          u        = fromIntegral x :: Int32
+          v        = fromIntegral y :: Int32
+{-# INLINE wordsToDouble #-}
+
+nextD :: PCG32 -> (Double, PCG32)
+nextD (PCG32 s inc) = 
+  let Pair s'  w1 = pair (PCG32 s inc)
+      Pair s'' w2 = pair (PCG32 s' inc)
+  in (wordsToDouble w1 w2, PCG32 s'' inc)
+{-# INLINE nextD #-}
+
+bounded' :: Word32 -> PCG32 -> Pair
+bounded' b (PCG32 s0 inc) = go s0
+  where
+   t = negate b `mod` b
+   go !s | r >= t    = Pair s' (r `mod` b)
+         | otherwise = go s'
+     where Pair s' r = pair (PCG32 s inc)
+{-# INLINE bounded' #-}
+
+-- | Generate a uniform 'Word32' less than the bound. If the bound is
+--   zero, this throws a divide by zero exception.
+bounded :: Word32 -> PCG32 -> (Word32, PCG32)
+bounded = \b g@(PCG32 _ inc) ->
+  let !(Pair s1 w) = bounded' b g
+  in  (w, PCG32 s1 inc)
+{-# INLINE bounded #-}
+
+-- Utilities -----------------------------------------------------------
+
+-- | Split the generator @g@ into @(g', g2)@ where @g'@ is @g@
+--   incremented by 4 steps and @g2@ is a new generator with a
+--   difference sequence to @g@.
+split :: PCG32 -> (PCG32, PCG32)
+split (PCG32 s inc) = (PCG32 s4 inc, mk w1 w2 w3 w4)
+  where
+    -- This is just something I made up. It passed big crunch and
+    -- dieharder (by splitting every step) but there's probably a better
+    -- way.
+    mk a b c d = newPCG32 (wordsTo64Bit a b) (wordsTo64Bit c d)
+    Pair s1 w1 = pair (PCG32 s  inc)
+    Pair s2 w2 = pair (PCG32 s1 inc)
+    Pair s3 w3 = pair (PCG32 s2 inc)
+    Pair s4 w4 = pair (PCG32 s3 inc)
+{-# INLINE split #-}
+
+advancing
+  :: Word64 -- amount to advance by
+  -> Word64 -- state
+  -> Word64 -- multiplier
+  -> Word64 -- increment
+  -> Word64 -- new state
+advancing d0 s m0 p0 = go d0 m0 p0 1 0
+  where
+    go !d !cm !cp !am !ap
+      | d <= 0    = am * s + ap
+      | odd d     = go d' cm' cp' (am * cm) (ap * cm + cp)
+      | otherwise = go d' cm' cp' am        ap
+      where
+        cm' = cm * cm
+        cp' = (cm + 1) * cp
+        d'  = d `div` 2
+{-# INLINE advancing #-}
+
+-- | Advance a pcg generator @n@ steps. You can give this @-n@ to take
+--   the generator back @n@ steps.
+advancePCG32 :: Word64 -> PCG32 -> PCG32
+advancePCG32 = \d (PCG32 s inc) -> PCG32 (advancing d s multiplier inc) inc
+{-# INLINE advancePCG32 #-}
+
+-- Misc ----------------------------------------------------------------
+
+wordsTo64Bit :: Integral a => Word32 -> Word32 -> a
+wordsTo64Bit x y =
+    fromIntegral ((fromIntegral x `shiftL` 32) .|. fromIntegral y :: Word64)
+{-# INLINE wordsTo64Bit #-}
+
